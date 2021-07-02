56f047d45db0120ac56385c12c3eefb0151b918f
Amal J Alappat <amalalappattofficial@gmail.com>
2021-07-02 22:39:46 +0530

    search button edited

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var count=0;
+
 $(".button_style").click(function(){
-  $(".search_expand").removeClass("search_invisible");
-});
+  count++;
+ output();
+  });
+
+
+  function output(){
+    if(count%2!==0){
+      $(".search_expand").removeClass("search_invisible");
+      console.log("expand");
+    }
+    if(count%2===0){
+      $(".search_expand").addClass("search_invisible");
+      console.log("contract");
+    }
+
+  }
